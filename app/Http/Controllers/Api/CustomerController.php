7476879d393953baeced827b9faae1d8a3069e0f
Farhad Abbasov <farhad.abbasov2003@gmail.com>
2025-01-06 14:42:42 +0400

    new files added

---
+++ app/Http/Controllers/Api/CustomerController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $customers = Customer::all();
+
+        return response()->json($customers);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Customer::create($request->only([
+            'customerNumber',
+            'customerName',
+            'contactLastName',
+            'contactFirstName',
+            'phone',
+            'addressLine1',
+            'addressLine2',
+            'city',
+            'state',
+            'postalCode',
+            'country',
+            'salesRepEmployeeNumber',
+            'creditLimit',
+        ]));
+
+        return response()->json("Customer store");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+       return Customer::where("customerNumber",$id)->first();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+
+        Customer::where("customerNumber",$id)->first()->update($request->only([
+            'customerNumber',
+            'customerName',
+            'contactLastName',
+            'contactFirstName',
+            'phone',
+            'addressLine1',
+            'addressLine2',
+            'city',
+            'state',
+            'postalCode',
+            'country',
+            'salesRepEmployeeNumber',
+            'creditLimit',
+        ]));
+
+        return response()->json("Customer update");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Customer::where("customerNumber",$id)->first()->delete();
+
+        return response()->json("Customer destroy");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\EmployeeController;
+use App\Http\Controllers\Api\OfficeController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\OrderDetailController;
+use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ProductLineController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +25,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('customers', CustomerController::class);
+Route::apiResource('employees', EmployeeController::class);
+Route::apiResource('offices', OfficeController::class);
+Route::apiResource('orderdetails', OrderDetailController::class);
+Route::apiResource('orders', OrderController::class);
+Route::apiResource('payments', PaymentController::class);
+Route::apiResource('productlines', ProductLineController::class);
+Route::apiResource('products', ProductController::class);
